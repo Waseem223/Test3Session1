19363a33e08e0c7ddc4e2290ebbab6c8db947970
Waseem223 <93112692+Waseem223@users.noreply.github.com>
2022-03-07 06:26:33 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,32 @@
-
+/*<br> 1.	Write a program to find sum of two fractions
+	<br> void input(int \*num1, int \*den1, int \*num2, int \*den2);
+	<br> void add(int num1,int den1, int num2, int den2, int \*num3, int \*den3);
+	<br> void output(int num1, int den1, int num2, int den2, int num3, int den3);
+	<br> input:
+	<br> 1 4
+	<br> 1 2
+	<br> output:
+	<br> 1/2 + 1/4 = 3/4*/
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("enter numerator 1 , enter denominator 1 , enter numerator 2 , enter denominator 2 \n");
+  scanf("%d%d%d%d",num1,den1,num2,den2);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  *num3=(num1*den2)+(num2*den1);
+  *den3=den1*den2;
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("the sum of %d/%d + %d/%d = %d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2, den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
 }
---
+++ p3original.c
@@ -0,0 +1,24 @@
+/*<br> 3.	Write a program find the nCr given n and r. 
+	<br> int input_n_and_r(int \*n, int \*r)
+	<br> int ncr(int n, int r);
+	<br> int ncr(int n, int r);*/
+#include<stdio.h>
+void input_n_and_r(int *n,int *r)
+{
+  printf("enter the values of n,r\n");
+  scanf("%d%d",n,r);
+}
+int ncr(int n, int r)
+{
+  int result;
+  result=(fact(n)/fact(n-r);
+  return result;
+}
+int ncr(int n, int r)
+int main()
+{
+  int n,r;
+  input_n_and_r(&n,&r);
+  int ncr(n, r);
+  int ncr(n,r);
+}
