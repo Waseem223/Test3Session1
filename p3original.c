53c74afbf774f59f4165823a4c976898a71d8140
Waseem223 <93112692+Waseem223@users.noreply.github.com>
2022-03-07 06:54:52 +0000

    done

--- p3original.c
+++ p3original.c
@@ -1,24 +1,34 @@
-/*<br> 3.	Write a program find the nCr given n and r. 
-	<br> int input_n_and_r(int \*n, int \*r)
-	<br> int ncr(int n, int r);
-	<br> int ncr(int n, int r);*/
 #include<stdio.h>
-void input_n_and_r(int *n,int *r)
+int input_n_and_r(int *n,int *r)
 {
-  printf("enter the values of n,r\n");
-  scanf("%d%d",n,r);
+    printf("enter n and r value \n");
+    scanf("%d%d",n,r);
 }
-int ncr(int n, int r)
+int fact(int r)
 {
-  int result;
-  result=(fact(n)/fact(n-r);
-  return result;
+    int n,i,v=1;
+    for(i=r;i>=1;i--)
+    {
+        v=v*i;
+    }
+    return v;
 }
-int ncr(int n, int r)
+int ncr(int n,int r)
+{
+    int result;
+    result=fact(n)/(fact(r)*fact(n-r));
+    return result;
+}
+void output(int n,int r,int result)
+{
+    printf("the ncr value for n= %d , r= %d is %d",n,r,result);
+}
 int main()
 {
-  int n,r;
-  input_n_and_r(&n,&r);
-  int ncr(n, r);
-  int ncr(n,r);
+    int n,r,result,num;
+    input_n_and_r(&n,&r);
+    result=ncr(n,r); 
+    output(n,r,result);
+    return 0;
+
 }
